5d42f10d7d375db67a32a7e2a944e39eebb78697
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2024-05-02 10:23:49 -0500

    added  stats fields and static values

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// set up stat variables
+
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeaponIndex = 0;
+let fighting = false;
+let monsterHealth = 100;
+let inventory = ["stick", "rubber boot", "Bucket"]
